024f1799faab8c9eb2538c0b337376ae5e4de6dc
Joseph Pietruch <percipient24@gmail.com>
2016-05-19 22:48:41 -0400

    Lots of Art Files, + a first build test.
    Adds the source textures for the current set of game assets.
    Adds gradlew (in case one doesn't have system gradle installed).
    Builds to package/assets/

--- src/main/java/com/percipient24/cgc/art/Textures.java
+++ src/main/java/com/percipient24/cgc/art/Textures.java
@@ -1,8 +1,27 @@
 package com.percipient24.cgc.art;
 
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker.Settings;
+
 public class Textures {
-	
 	public static void main(String[] args) {
-		System.out.println("Hello, world!");
+		Settings settings = new Settings();
+		settings.maxWidth = 2048;
+		settings.maxHeight = 2048;
+
+		// We will assume that this code runs from
+		// [art]
+		// Raw assets live in
+		// [art]/assets/[sheetname]
+		// Thus, all asset source paths should be prefixed with
+		// assets
+		// and all asset destination paths should be prefixed with
+		// package
+
+		TexturePacker.process(settings, "assets/images", "package/assets", "main");
+		TexturePacker.process(settings, "assets/characters", "package/assets", "characters");
+		TexturePacker.process(settings, "assets/titleScreen", "package/assets", "titlescreen");
+		TexturePacker.process(settings, "assets/menuControls", "package/assets", "menu");
+		TexturePacker.process(settings, "assets/creditslogos", "package/assets", "credits");
 	}
 }
